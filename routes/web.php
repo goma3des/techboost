04ee4563cd211b70495576c75d5040f75cb6049b
kyoko <kyokoueno4biz@gmail.com>
2020-05-14 22:10:23 +0900

    Routing assignment

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//3.「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」
+Route::get('xxx/AAA/bbb', 'xxx\AAAController@bbb');
+
+//4.
+
+Route::group(['prefix' => 'admin'], function(){
+  Route::get('news/create', 'admin\NewsController@add');
+  Route::get('profile/create', 'admin\ProfileController@add');
+  Route::get('profile/edit', 'admin\profileController@edit');
+});
