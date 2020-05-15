aeacd4ea908d78a688aa998aa8716cce8a97657a
kyoko <kyokoueno4biz@gmail.com>
2020-05-15 23:10:08 +0900

    create blade.php

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=devide-width, initial-scale=1">
+  </head>
+
+  <title>MyNews</title>
+<body>
+  <h2>プロフィール追加画面</h2>
+</body>
+</html>
